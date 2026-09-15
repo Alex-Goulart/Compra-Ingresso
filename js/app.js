33f37ed77c0af9ceb9ed557b1a26d2a7369d3fcc
Alex-Goulart <alex.m.goulart@hotmail.com>
2026-09-15 20:16:58 -0300

    Atualização da Função Comprar(Atualizar o valor dos tipos de imgresso e limpar a quantidade apos cada compra)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
-// criar uma função para compra de emgresos
+// Função para realizar a compra
 function comprar() {
-  
+    // recuperar o tipo de ingresso selecionado
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    // recuperar a quantidade de ingressos selecionada
+    let quantidade = Number(document.getElementById('qtd').value);
+
+    // Verificar o tipo de ingresso escolhido
+    if (tipoIngresso === 'pista') {
+        let quantidadeDisponivel = Number(document.getElementById('qtd-pista').textContent); // recuperar a quantidade de ingressos disponíveis para pista
+        if (quantidade <= quantidadeDisponivel) {
+            quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
+            document.getElementById('qtd-pista').textContent = quantidadeDisponivel;
+            alert(`Compra realizada com sucesso! Restam ${quantidadeDisponivel} ingressos de pista.`);
+        } else {
+            alert('Quantidade de ingressos selecionada excede a disponível.');
+        }
+    }else if (tipoIngresso === 'superior') {
+        let quantidadeDisponivel = Number(document.getElementById('qtd-superior').textContent); // recuperar a quantidade de ingressos disponíveis para superior
+        if (quantidade <= quantidadeDisponivel) {
+            quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
+            document.getElementById('qtd-superior').textContent = quantidadeDisponivel;
+            alert(`Compra realizada com sucesso! Restam ${quantidadeDisponivel} ingressos de superior.`);
+        } else {
+            alert('Quantidade de ingressos selecionada excede a disponível.');
+        }
+    } else if (tipoIngresso === 'inferior') {
+        let quantidadeDisponivel = Number(document.getElementById('qtd-inferior').textContent); // recuperar a quantidade de ingressos disponíveis para inferior
+        if (quantidade <= quantidadeDisponivel) {
+            quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
+            document.getElementById('qtd-inferior').textContent = quantidadeDisponivel;
+            alert(`Compra realizada com sucesso! Restam ${quantidadeDisponivel} ingressos de inferior.`);
+        } else {
+            alert('Quantidade de ingressos selecionada excede a disponível.');
+        }
+    }
+
+    //limpar o campo de quantidade após a compra
+    document.getElementById('qtd').value = '';
 }
