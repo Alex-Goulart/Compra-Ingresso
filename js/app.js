6d7dab8a4ca8ce51b8ee19963b46428c97182750
Alex-Goulart <alex.m.goulart@hotmail.com>
2026-09-17 19:28:04 -0300

    Nova Atualização (Retirada de Alguns Alerts)

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ function comprar() {
     // recuperar o tipo de ingresso selecionado
     let tipoIngresso = document.getElementById('tipo-ingresso').value;
     // recuperar a quantidade de ingressos selecionada
-    let quantidade = Number(document.getElementById('qtd').value);
+    let quantidade = parseInt(document.getElementById('qtd').value);
 
     // Verificar o tipo de ingresso escolhido
     if (tipoIngresso === 'pista') {
-        let quantidadeDisponivel = Number(document.getElementById('qtd-pista').textContent); // recuperar a quantidade de ingressos disponíveis para pista
+        let quantidadeDisponivel = parseInt(document.getElementById('qtd-pista').textContent); // recuperar a quantidade de ingressos disponíveis para pista
         if (quantidade <= quantidadeDisponivel) {
             quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
             document.getElementById('qtd-pista').textContent = quantidadeDisponivel;
@@ -15,7 +15,7 @@ function comprar() {
             alert('Quantidade de ingressos selecionada excede a disponível.');
         }
     }else if (tipoIngresso === 'superior') {
-        let quantidadeDisponivel = Number(document.getElementById('qtd-superior').textContent); // recuperar a quantidade de ingressos disponíveis para superior
+        let quantidadeDisponivel = parseInt(document.getElementById('qtd-superior').textContent); // recuperar a quantidade de ingressos disponíveis para superior
         if (quantidade <= quantidadeDisponivel) {
             quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
             document.getElementById('qtd-superior').textContent = quantidadeDisponivel;
@@ -23,7 +23,7 @@ function comprar() {
             alert('Quantidade de ingressos selecionada excede a disponível.');
         }
     } else if (tipoIngresso === 'inferior') {
-        let quantidadeDisponivel = Number(document.getElementById('qtd-inferior').textContent); // recuperar a quantidade de ingressos disponíveis para inferior
+        let quantidadeDisponivel = parseInt(document.getElementById('qtd-inferior').textContent); // recuperar a quantidade de ingressos disponíveis para inferior
         if (quantidade <= quantidadeDisponivel) {
             quantidadeDisponivel = quantidadeDisponivel - quantidade; // atualizar a quantidade de ingressos disponíveis
             document.getElementById('qtd-inferior').textContent = quantidadeDisponivel;
