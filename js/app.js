48717ee2608979345d311a53399840643b1ca7ef
Alex-Goulart <alex.m.goulart@hotmail.com>
2026-09-17 20:25:45 -0300

    Atualização V5.170924 (Validação da quantidade e Validação da Compra)

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ function comprar() {
     // recuperar a quantidade de ingressos selecionada
     let quantidade = parseInt(document.getElementById('qtd').value);
 
+    //Validação da quantidade de ingressos
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert('Por favor, insira uma quantidade válida de ingressos.');
+        return;
+    }
+
+    // Confirma a compra
+    let confirmarCompra = confirm(`Você deseja comprar ${quantidade} ingresso(s) do tipo ${tipoIngresso}?`);
+    if (!confirmarCompra) {
+        return; // Se o usuário cancelar, a função é encerrada
+    }
+
     // Verificar o tipo de ingresso escolhido
     if (tipoIngresso === 'pista') {
         let quantidadeDisponivel = parseInt(document.getElementById('qtd-pista').textContent); // recuperar a quantidade de ingressos disponíveis para pista
